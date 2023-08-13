37ad9f243954343d79c679699c0af1c0e0c9c0ae
Simamnkele <smambo85@gmail.com>
2023-08-13 22:16:05 +0200

    Fixed edge case error

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,18 +2,15 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     ta = tuple_a
     tb = tuple_b
-    if len(ta) < 2:
-        ta = (0, 0)
-    elif len(ta) > 2:
-        ta = (tuple_a[0], tuple_a[1])
-    elif len(ta) == 1:
-        ta = (tuple_a[0], 0)
 
-    if len(tb) < 2:
-        tb = (0, 0)
-    elif len(tb) > 2:
-        tb = (tuple_b[0], tuple_b[1])
-    elif len(tb) == 1:
+    if len(ta) == 1:
+        ta = (tuple_a[0], 0)
+    elif len(ta) < 2:
+        ta = (0, 0)
+
+    if len(tb) == 1:
         tb = (tuple_b[0], 0)
+    elif len(tb) < 2:
+        tb = (0, 0)
 
     return (ta[0] + tb[0], ta[1] + tb[1])
