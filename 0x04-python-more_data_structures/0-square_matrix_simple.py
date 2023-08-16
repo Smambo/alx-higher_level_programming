26d5b812d3a203661859d99037873ba860a7558d
Simamnkele <smambo85@gmail.com>
2023-08-16 19:10:12 +0200

    Fixing checker error

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     if matrix:
+        if len(matrix) == 0:
+            return None
         result = []
         for element in matrix:
             row = []
