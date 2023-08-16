b543b5e98f5cf7fed3a3f995876ab7a767012737
Simamnkele <smambo85@gmail.com>
2023-08-16 21:16:31 +0200

    Changed to nested list comprehension

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if len(matrix) == 0:
-        return None
-    result = []
-    for element in matrix:
-        row = []
-        for num in element:
-            row.append(num*num)
-        result.append(row)
-    return(result)
+    return [[num*num for num in element] for element in matrix]
