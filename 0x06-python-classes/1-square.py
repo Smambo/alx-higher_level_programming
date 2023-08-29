b71631cabb53a298f39cd42781cb41f7d55640af
Simamnkele <smambo85@gmail.com>
2023-08-29 20:50:43 +0200

    Added task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Square:
+    """
+    creates a square class object
+    """
+    def __init__(self, size):
+        self.__size = size
+    """
+    initialise square class object
+    with parameter
+    __size: size of square
+    """
