f8dc1960c6bac952987a032da3ea3eaf96db7278
Simamnkele <smambo85@gmail.com>
2023-09-04 21:50:13 +0200

    Added task 3

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Below class module defines a rectangle."""
+
+
+class Rectangle:
+    """Creates a rectangle class object."""
+    def __init__(self, width=0, height=0):
+        """Initialises instance of rectangle
+        Args:
+            __width(int): width of rectangle
+            __height(int): height of rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieves width."""
+        return self.__width  # private instance
+
+    @property
+    def height(self):
+        """Retrieves height."""
+        return self.__height  # private instance
+
+    @width.setter
+    def width(self, value):
+        """Sets width."""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """Sets height."""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Returns rectangle area."""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Returns rectangle perimeter."""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return ((self.width * 2) + (self.height * 2))
+
+    def __str__(self):
+        """Modifies str object."""
+        if self.width == 0 or self.height == 0:
+            return ""
+        return ('\n'.join('#' * self.width for n in range(self.height)))
