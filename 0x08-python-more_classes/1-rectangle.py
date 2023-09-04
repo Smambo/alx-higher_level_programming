d9a0040ef3fa49dd48d24fe26c0306cc3d864ef7
Simamnkele <smambo85@gmail.com>
2023-09-04 20:13:52 +0200

    Changes to order of function defs

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -20,6 +20,11 @@ class Rectangle:
         """Retrieves width."""
         return self.__width
 
+    @property
+    def height(self):
+        """Retrieves height."""
+        return self.__height
+
     @width.setter
     def width(self, value):
         """Sets width."""
@@ -28,11 +33,6 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-
-    @property
-    def height(self):
-        """Retrieves height."""
-        return self.__height
 
     @height.setter
     def height(self, value):
