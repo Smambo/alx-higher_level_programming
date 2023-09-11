10e304931dfe7828f6cf8a6524545c2868fe1400
Simamnkele <smambo85@gmail.com>
2023-09-11 19:04:22 +0200

    Fixing checker errors

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -10,6 +10,6 @@ def is_same_class(obj, a_class):
     """Returns True if exactly an instance
     otherwise False.
     """
-    if isinstance(obj, a_class):
+    if type(obj) is a_class:
         return True
     return False
