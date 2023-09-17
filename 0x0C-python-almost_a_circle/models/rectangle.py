757dd561bf41d42a887ba248a8dff56039787958
Simamnkele <smambo85@gmail.com>
2023-09-17 21:04:50 +0200

    Updated class by adding validator function for methods

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -15,6 +15,15 @@ class Rectangle(Base):
         self.y = y
         super().__init__(id)
 
+    def validator(self, name, value):
+        """Validates other methods."""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if (name == "width" or name == "height") and value <= 0:
+            raise ValueError("{} must be > 0".format(name))
+        if (name == "x" or name == "y") and value < 0:
+            raise ValueError("{} must be >= 0".format(name))
+
     @property
     def width(self):
         """Retrieves width."""
@@ -38,19 +47,23 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Sets width."""
+        self.validator("width", value)
         self.__width = value
 
     @height.setter
     def height(self, value):
         """Sets height."""
+        self.validator("height", value)
         self.__height = value
 
     @x.setter
     def x(self, value):
         """Sets x attr."""
+        self.validator("x", value)
         self.__x = value
 
     @y.setter
     def y(self, value):
         """Sets y attr."""
+        self.validator("y", value)
         self.__y = value
