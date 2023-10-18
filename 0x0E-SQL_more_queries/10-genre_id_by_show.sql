33941d17a533c6d8c7b1a3cd929817fb7613313e
Simamnkele <smambo85@gmail.com>
2023-10-18 22:52:35 +0200

    Removed line

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,5 @@
 -- script lists all shows contained in 'hbtn_0d_tvshows'
 -- that have at least one genre linked
-USE hbtn_0d_tvshows;
 SELECT ts.title, tg.genre_id FROM tv_shows ts, tv_show_genres tg
 WHERE ts.id = tg.show_id
 ORDER BY ts.title ASC, tg.genre_id ASC;
