964de63174e2bbf59e8b974792da326c1be20ee4
Simamnkele <smambo85@gmail.com>
2023-10-18 23:15:57 +0200

    Pushed remaining tasks

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- script uses database to list
+-- all genres of the show 'Dexter'
+SELECT tgg.name FROM tv_genres AS tgg
+JOIN tv_show_genres AS tgs
+ON tgs.genre_id = tgg.id
+JOIN tv_shows AS ts
+ON ts.id = tgs.show_id
+WHERE ts.title = 'Dexter'
+GROUP BY tgg.name ASC;
