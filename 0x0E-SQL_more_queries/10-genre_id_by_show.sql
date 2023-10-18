8f4aaaae99b50e982456a9b2aa1bd55b0cbbb78c
Simamnkele <smambo85@gmail.com>
2023-10-18 22:30:55 +0200

    Updated for checker errors

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,6 @@
 -- script lists all shows contained in 'hbtn_0d_tvshows'
 -- that have at least one genre linked
-SELECT tv_shows.title, tv_shows_genre.genre_id FROM tv_shows, tv_shows_genre
-WHERE tv_shows.id = tv_shows_genre.show_id
-ORDER BY tv_shows.title ASC, tv_shows_genre.genre_id ASC;
+USE hbtn_0d_tvshows;
+SELECT ts.title, tg.genre_id FROM tv_shows ts, tv_shows_genre tg
+WHERE ts.id = tg.show_id
+ORDER BY ts.title ASC, tg.genre_id ASC;
