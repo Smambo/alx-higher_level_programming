8f3785f5fb6b3e067c2b50427052ff94da5a4fe2
Simamnkele <smambo85@gmail.com>
2023-10-18 22:56:35 +0200

    Fixing checker errors

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- script lists all cities contained
 -- in database 'hbtn_0d_usa'
-SELECT cities.id, cities.name, states.name FROM cities, states
-WHERE cities.state_id = states.id
-GROUP BY cities.id ASC;
+SELECT c.id, c.name, s.name FROM cities c, states s
+WHERE c.state_id = s.id
+GROUP BY c.id ASC;
