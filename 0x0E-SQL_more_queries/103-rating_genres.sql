8a94257a60fe21de62f8c7995e97bf916ee90236
Simamnkele <smambo85@gmail.com>
2023-10-19 21:18:15 +0200

    Added advanced tasks

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,7 @@
+-- script lists all genres by their ratings
+SELECT tg.name, SUM(tr.rate) AS rating FROM tv_show_genres tgs
+JOIN tv_genres tg
+ON tg.id = tgs.genre_id
+JOIN tv_show_ratings tr
+ON tr.show_id = tgs.show_id
+GROUP BY tg.name DESC ORDER BY RATING DESC;
