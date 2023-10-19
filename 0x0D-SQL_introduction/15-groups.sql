42dbf35e62d090e21f84cb9d47c363d4ef9c5df6
Simamnkele <smambo85@gmail.com>
2023-10-19 18:45:22 +0200

    Refactored code to fix errors

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,5 @@
 -- script lists number of records with
 -- same score in the table
-SELECT `score`, COUNT(`score`) AS number FROM `second_table`
+SELECT `score`, COUNT(`score`) AS number
+FROM `second_table`
 GROUP BY `score` DESC;
