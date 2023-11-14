19045dab28472e3501ef9ad8561f1af11212a86d
Simamnkele <smambo85@gmail.com>
2023-11-14 20:02:31 +0200

    Fixed semistandard errors

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,7 +5,7 @@ class Square extends square {
   charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
-	}
+    }
   }
 }
 module.exports = Square;
