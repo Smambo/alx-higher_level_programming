84c00da1e51afdb42ad2cb1a55cd881444070c31
Simamnkele <smambo85@gmail.com>
2023-11-15 11:40:01 +0200

    Refactored code to account for double-digit integers

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,5 +5,5 @@ const args = process.argv.slice(2).map(x => Number(x));
 if (args.length === undefined || args.length <= 1) {
   console.log(0);
 } else {
-  console.log(args.sort().reverse()[1]);
+  console.log(args.sort((a, b) => b - a)[1]);
 }
