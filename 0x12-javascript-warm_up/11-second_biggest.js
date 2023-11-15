6b8a8ce2abbde0716e19d75357eb5f7e392be140
Simamnkele <smambo85@gmail.com>
2023-11-15 11:21:32 +0200

    Refactored code for more edge cases

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 
 const args = process.argv.slice(2).map(x => Number(x));
 
-if (args.length === undefined || args.length <= 2) {
+if (args.length === undefined || args.length <= 1) {
   console.log(0);
 } else {
   console.log(args.sort().reverse()[1]);
