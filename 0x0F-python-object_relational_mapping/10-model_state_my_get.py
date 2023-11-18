9ecba0c835e6851b67853e228ac392f6960a5b0a
Simamnkele <smambo85@gmail.com>
2023-11-18 13:26:52 -0800

    Pushed script that prints State object with name passed as argument

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Import modules for script.
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy import MetaData
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.format(
+                sys.argv[1],
+                sys.argv[2],
+                sys.argv[3]),
+            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    sesh = Session()
+    state = sesh.query(State).filter(State.name == sys.argv[4]).first()
+    try:
+        print("{}".format(state.id))
+    except:
+        print("Not found")
+    sesh.close()
