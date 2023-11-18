4f070779f570523c86287da0be886b3d40670cf7
Simamnkele <smambo85@gmail.com>
2023-11-18 01:56:24 -0800

    Pushed script that lists all states from database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Import modules for script.
+"""
+import sys
+import MySQLdb
+
+
+def main(args):
+    """Script lists all states from database."""
+    if len(args) != 4:
+        raise Exception("3 arguments needed!")
+    db = MySQLdb.connect(
+            host='localhost',
+            user=args[1],
+            passwd=args[2],
+            db=args[3]
+            )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
