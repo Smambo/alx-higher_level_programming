cabcbd5a0a4c4685c23dfe9dc467f4428d63c1c5
Simamnkele <smambo85@gmail.com>
2023-11-18 07:19:30 -0800

    Pushed script that displays values where name matches argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Import modules for script.
+"""
+import sys
+import MySQLdb
+
+
+def main(args):
+    """
+    Script displays all values in database table
+    where name matches the argument.
+    """
+    if len(args) != 5:
+        raise Exception("4 arguments needed!")
+    db = MySQLdb.connect(
+            host='localhost',
+            user=args[1],
+            passwd=args[2],
+            db=args[3]
+            )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC")
+    states = cur.fetchall()
+    for state in states:
+        if state[1] == args[4]:
+            print(state)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
