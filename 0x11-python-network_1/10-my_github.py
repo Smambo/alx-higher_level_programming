7bf5551d71918bc9db0c11cb7b1abee1e0151fc7
Simamnkele <smambo85@gmail.com>
2023-12-02 20:14:41 +0200

    Changed credentials

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,7 +7,7 @@ import requests
 
 
 try:
-    req = requests.get("https://api.github.com/Smambo",
+    req = requests.get("https://api.github.com/user",
                        auth=(argv[1], argv[2]))
     print(req.json().get('id'))
 except Exception:
