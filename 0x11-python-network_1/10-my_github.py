06d0ba3be8689f787c837c693daafd16dfdf93e0
Simamnkele <smambo85@gmail.com>
2023-12-02 20:13:20 +0200

    Added github task

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Below script displays your id using the Github API
+"""
+from sys import argv
+import requests
+
+
+try:
+    req = requests.get("https://api.github.com/Smambo",
+                       auth=(argv[1], argv[2]))
+    print(req.json().get('id'))
+except Exception:
+    print("None")
