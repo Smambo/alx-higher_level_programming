0a5e8c1ba0fdf22d1cda689906766e8576876e4c
Simamnkele Njoli <smambo85@gmail.com>
2024-01-03 04:12:24 -0800

    Removed comment

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 
-//Script prints the title of a Star Wars movie where the episode number matches a given integer
 const request = require('request');
 const num = process.argv[2];
 
